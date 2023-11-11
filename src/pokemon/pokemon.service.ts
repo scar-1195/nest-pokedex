e9f4f37085003d5d65791b29db3d9053d6ac1f49
oscar <o.lrodriguez1195@gmail.com>
2023-11-10 21:17:31 -0600

    feat(pokemonFindAll):create endpoint with pagination a query params DTO

--- src/pokemon/pokemon.service.ts
+++ src/pokemon/pokemon.service.ts
@@ -10,6 +10,7 @@ import { Model, isValidObjectId } from 'mongoose';
 import { Pokemon } from './entities/pokemon.entity';
 import { CreatePokemonDto } from './dto/create-pokemon.dto';
 import { UpdatePokemonDto } from './dto/update-pokemon.dto';
+import { PaginationDto } from 'src/common/dto/pagination.dto';
 
 @Injectable()
 export class PokemonService {
@@ -28,8 +29,9 @@ export class PokemonService {
     }
   }
 
-  findAll() {
-    return `This action returns all pokemon`;
+  findAll(paginationDto: PaginationDto) {
+    const { limit = 10, offset = 0 } = paginationDto;
+    return this.pokemonModel.find().limit(limit).skip(offset).select('-__v');
   }
 
   async findOne(term: string) {
