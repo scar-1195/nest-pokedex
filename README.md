c018b44f6b14bb60683ec72c02e18c186e339fe4
oscar <o.lrodriguez1195@gmail.com>
2023-11-13 20:42:38 -0600

    feat(env_config):.env created add validation joi schema for .env

--- README.md
+++ README.md
@@ -1,30 +1,46 @@
 <p align="center">
-  <a href="http://nestjs.com/" target="blank"><img src="https://nestjs.com/img/logo-small.svg" width="200" alt="Nest Logo" /></a>
+   <a href="http://nestjs.com/" target="blank"><img src="https://nestjs.com/img/logo-small.svg" width="200" alt="Nest Logo" /></a>
 </p>
 
-# Ejecutar en desarrollo
+# Run App (dev)
 
-1. Clonar el repositorio.
-
-2. Ejecutar.
+1. Clone the repository
+2. Run
 
 ```
 yarn install
 ```
 
-3. Tener Nest CLI instalado.
+3. Have Nest CLI installed
 
 ```
 npm i -g @nestjs/cli
 ```
 
-4. Levantar la base de datos.
+4. Raise the database
 
 ```
 docker-compose up -d
 ```
 
-## Stack usado
+5. Clone the file `.env.example` and rename the copy to `
+.env`
+
+6. Fill the environment variables defined in the `.env`
+
+7. Run the application in dev:
+
+```
+yarn start:dev
+```
+
+8. Rebuild the database with the seed
+
+```
+http://localhost:3000/api/v2/seed
+```
+
+## Stack used
 
 - MongoDB
 - Nest
--- src/app.module.ts
+++ src/app.module.ts
@@ -2,16 +2,28 @@ import { join } from 'path';
 import { Module } from '@nestjs/common';
 import { ServeStaticModule } from '@nestjs/serve-static';
 import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule } from '@nestjs/config';
+
 import { PokemonModule } from './pokemon/pokemon.module';
 import { CommonModule } from './common/common.module';
 import { SeedModule } from './seed/seed.module';
+import { AppConfig } from './config/app.config';
+import { JoiValidationSchema } from './config/joi.validation';
 
 @Module({
   imports: [
+    ConfigModule.forRoot({
+      load: [AppConfig],
+      validationSchema: JoiValidationSchema,
+    }),
+
     ServeStaticModule.forRoot({
       rootPath: join(__dirname, '..', 'public'),
     }),
-    MongooseModule.forRoot('mongodb://localhost:27017/nest-pokemon'),
+
+    MongooseModule.forRoot(process.env.MONGODB, {
+      dbName: 'NestPokemonDB',
+    }),
     PokemonModule,
     CommonModule,
     SeedModule,
